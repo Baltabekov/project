c99dd5c8d30df20aa86b9b28aaca707c71b44f4c
Baltabekov <baltabekov.edige@mail.ru>
2016-05-14 00:23:35 +0600

    Add files via upload

---
+++ euro.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+<title>Қазақстан және Нидерланды</title>
+<style type="text/css">
+		.main{
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		width: 100%;
+		height: 40px;
+		background-color: black;
+		position: fixed;
+}
+.usti{
+	float: left;
+	}
+	body{
+		background-image:url("background.jpg");
+		background-position: center;
+		background-attachment: fixed;
+		background-size: 100%;
+	}
+	.kz{
+	margin-top:40px;
+    margin-right: auto;
+    margin-left: auto;
+    width:70%;
+    height:2800px;
+    background-color: white;
+}
+
+.footer{
+	color:white;
+	float:left;
+
+}
+
+.foot{
+	position: absolute;
+	height:80px;
+	background-color: black;
+	width: 100%;
+	left: 0px;
+	right: 0px;
+}
+
+
+
+</style>
+</head>
+<body>
+<div class="main">
+<div class="ch" style="margin-top:-110px;"><img src="200px-UEFA_Champions_League_logo.svg.png" style="margin-left:200px;width:80px;height:80px;margin-top:30px;"></div>
+<div class="usti"><img src="Football_Federation_of_Kazakhstan_Logo.svg.png"style="margin-left:10px;width:60px;height:40px;"></div>
+<div class="usti" style="margin-left:0px;padding: 8px 20px;	font-style:italic;font-family:sans-serif;font-weight:800;font-size:23px;margin-top:-2px"><a href="mid1.php"style="color:yellow;text-decoration:none;">Қазақстан футбол федерациясы</a></div>
+<div class="usti" style="margin-left:500px;">
+<form style="margin-top:-7px;">
+   <p><center><input type="search" name="q" placeholder="Іздеу" style="width:200px;height:30px;margin-top:-10px;"> 
+   <input type="submit" value="Табу" style="color:black"></center></p>
+  </form>
+</div>
+<div class="usti" style="font-weight:300;font-family:sans-serif;font-stretch: expanded;margin-left:22px;padding:10px;color:#A9A9A9;"><a href="loginproject.php" style="color:#A9A9A9;text-decoration:none;">Кіру<img src="35514718.png"style="height:15px;width:20px;"></a></div>
+</div>
+
+<div class="kz">
+<h1 style="font-stretch:condensed;font-family:Times New Roman;font-size:32px;margin-top:10px;margin-left:10px;">Жаңалықтар</h1>
+<hr>
+<h1 style="font-family:Lucida Sans Unicode;margin-left:5px;margin-top:0px;">Қазақстан құрамасы Нидерланды футболшыларына есе жіберді.</h1>
+<img src="1443787458eliv7.jpg" style="width:70%;height:400px;margin-left:142px;">
+<h3 style="font-weight:bold;margin-left:37px;">Футболдан Қазақстан құрамасы 2016 жылы Францияда өтетін Еуропа чемпионатына іріктеу кезеңі аясында Нидерланды құрамасымен кездесіп, 3:1 есебімен есе жіберді.</h3>   
+<h4 style="font-weight:200;font-size:17px;margin-left:37px;">«Амстердам Арена» стадионында өткен ойын аса тартысты болды. Алаң иелері алғашқы минуттардан ойынды өз қолдарына алып, жерлестерімізді тықсыра бастады. Нидерландылық футболшылардың жойқын шабуылдарына бірде қорғаушыларымыз қарсылық көрсетсе, бірде қарағандылық «Шахтердің» қақпашысы Александр Мокин тойтарыс беріп жатты. Алғаш болып қазақстандық футболшылардың сирек жасаған қарымта шабуылдарының бірінде есеп ашылды. Ойынның 17-ші минутында бұрыштамадан көтерілген допты баспен қақпаға тура бағыттаған Ренат Абдуллин Қазақстан құрамасын алға шығарды. Бұл доп қазақстандықтардың биылғы Еуропа чемпионатына іріктеу кезеңіндегі алғашқы голы болды.<br><br>
+Екінші кезеңді Нидерланды құрамасының футболшылары белсенді бастады. Ал, Юрий Красножанның командасы өз кезегінде қарсы шабуылдарға иек артып отырды. Толассыз шабуылдармен Александр Мокин қақпасын нысанаға алған Гус Хиддинк шәкірттері дегендеріне жетіп тынды. Клаас-Ян Хунтелаардың шығуымен есеп теңесіп, нидерландылықтардың шабуылына серпін қосылды.</h4>
+<img src="2163232_w2_thb_610x350x2.jpg" style="width:70%;height:400px;margin-left:142px;">
+
+<h4 style="font-weight:200;font-size:17px;margin-left:37px;">Нидерланды футболшыларының голынан кейін, арада екі минут өткен кезде Бауыржан Жолшиев қызыл қағаз алып, алаңнан қуылды. Бұл жағдайдан кейін ойын алаң илерінің толықтай иелігіне көшті. Ойынның негізгі бөлігі отандастарымыздың жарты алаңында өрбіді.<br><br>
+Нидерланды құрамасы сапынан Ибрахим Афеллай мен Робин ван Перси көзге түсіп, өз командасына жеңіс сыйлады. «Манчестер Юнайтед» командасының шабуылшысы Робин ван Перси он бір метрлік айып добын сәтті орындады.</h4>
+<img src="1412976910071_wps_45_VI_Images_via_Press_Assoc.jpg" style="width:70%;height:400px;margin-left:142px;">
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Нидерланды — Қазақстан - 3:1 (0:1)</h3>
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Голдар:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:69px;margin-top:-37px;">Абдулин 17 (0:1), Хунтелаар 62 (1:1), Афеллай 82 (2:1), Ван Перси пенальтиден 89 (3:1)</h4>
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Ескертулер:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:102px;margin-top:-37px;">Абдулин 33, Дмитренко 55, Мирошниченко 89</h4>
+
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Қызыл қағаз:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:120px;margin-top:-37px;">Жолшиев 64</h4>
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Нидерланды:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:112px;margin-top:-37px;">Силессен, Ван дер Виль, Де Врей, Мартинс Индии (Фер 81), Блинд, Де Йонг (Хунтелаар 56), Афеллай, Снейдер, Ленс, Ван Перси (к), Роббен</h4>
+
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Қазақстан:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:98px;margin-top:-37px;">Мокин, Воротников, Абдулин, Дмитренко (Гурман 72), Мирошниченко, Суюмбаев, Карпович (к) (Коробкин 79), Шомко, Богданов, Жолшиев, Хижниченко (Нұрғалиев 90)</h4>
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">Бас төреші:</h3>
+<h4 style="font-weight:200;font-size:17px;margin-left:98px;margin-top:-37px;">Матей Юг (Словения)</h4>
+<h3 style="font-weight:bold;font-size:17px;margin-left:7px;">10 қыркүйек. Амстердам. «Амстердам Арена».</h3>
+<iframe width="854" height="480" style="margin-left:38px;"src="https://www.youtube.com/embed/KfhDAiqt5dg"></iframe>
+</div>
+
+<div class="foot">
+<h4 style="margin-top:0px;font-family:Times New Roman;font-weight:300px;font-size:18px;color:white;">Ақпараттық серіктестер</h4>
+<hr style="margin-top:-22px;width:1345px;margin-left:0px;border:1px solid yellow">
+<div class="footer" style="margin-left:170px;margin-top:-6px;"><a href="http://vesti.kz/"><img src="vst.png"style="border-radius:8px;"></a></div>
+<div class="footer" style="margin-left:120px;margin-top:-6px;"><a href="http://alaman.kz/"><img src="alamankz (1).png"style="border-radius:8px;height:50px;"></a></div>
+<div class="footer" style="margin-left:120px;margin-top:-6px;"><a href="http://www.info-tses.kz/"><img src="gazeta.png"style="border-radius:8px;height:50px;"></a></div>
+<div class="footer" style="margin-left:120px;margin-top:-6px;"><a href="http://egemen.kz/"><img src="egemen.png"style="border-radius:8px;height:50px;"></a></div>
+</div>
+
+</body>
+</html>
